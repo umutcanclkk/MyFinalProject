3837badbcd255a455c20e9025c782ba3e63f45ec
Umutcan <Umutcan@VENUS>
2023-08-03 13:41:21 +0300

    Add project files.

---
+++ DataAccess/Abstract/ICategoryDal.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DataAccess.Abstract
+{
+    public interface ICategoryDal:IEntityRepository<Category>
+    {
+        
+
+    }
+       
+}
---
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfProductDal : IProductDal
+    {
+
+        public void Add(Product entity)
+        {
+            using (MasterContext context = new MasterContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product entity)
+        {
+            using (MasterContext context = new MasterContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            using (MasterContext context = new MasterContext())
+            {
+                return context.Set<Product>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            using (MasterContext context = new MasterContext())
+            {
+                return filter == null
+                    ? context.Set<Product>().ToList()
+                    : context.Set<Product>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Product entity)
+        {
+            using (MasterContext context = new MasterContext())
+            {
+                var updateEntity = context.Entry(entity);
+                updateEntity.State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
